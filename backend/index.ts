bdbac045c273ec3a6dac94515fd8019730da4db3
Matheus Baldissara <baldissaramatheus@gmail.com>
2022-04-17 18:51:57 -0300

    Include default architecture and initial template for the frontend application

---
+++ backend/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+
+const port = 4000;
+const app = express();
+
+async function startServer() {
+  try {
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true}))
+    app.use((req, res, next) => {
+      res.header(
+        'Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization'
+      );
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+      res.header('Access-Control-Expose-Headers', 'Content-Range');
+      next();
+    });
+    // app.use('/', userRoutes);
+    app.listen(port, () => {
+      console.log(`Servidor sendo executado em http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+startServer();
---
+++ frontend/src/App.js
@@ -0,0 +1,68 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="container mx-auto mt-8">
+      <h1 className="text-2xl mb-4">Users list</h1>
+      <form className="mb-4">
+        <label>
+          <span class="text-sm">Search User</span>
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Type the user name, age or cellphone number"
+            className="
+              block
+              w-full
+              max-w-md
+              py-2
+              px-3
+              border-2
+              border-gray-600
+              text-sm
+              text-gray-800
+              rounded-lg
+              shadow-sm
+              focus:outline-none
+              focus:ring
+            "
+          >
+          </input>
+        </label>
+      </form>
+      <table className="table-fixed w-full shadow-lg">
+        <thead>
+          <tr className="children:p-2 text-gray-500 text-left border-2 border-gray-200">
+            <th className="">Name</th>
+            <th>Age</th>
+            <th>Phone</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="children:p-2 border-2 border-gray-200 text-gray-800">
+            <td>Malcolm Lockyer</td>
+            <td>91</td>
+            <td>123412345</td>
+            <td>1961</td>
+          </tr>
+          <tr className="children:p-2 border-2 border-gray-200 text-gray-800">
+            <td>Witchy Woman</td>
+            <td>51</td>
+            <td>123412345</td>
+            <td>1961</td>
+          </tr>
+          <tr className="children:p-2 border-2 border-gray-200 text-gray-800">
+            <td>Shining Star</td>
+            <td>18</td>
+            <td>123412345</td>
+            <td>1961</td>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  );
+}
+
+export default App;
